a8b1729ffbbd69778e72da9ec238bf62198ed865
donggu1105 <kbh11051@gmail.com>
2021-03-09 06:52:03 +0900

    json 연습좀

--- main.py
+++ main.py
@@ -1,17 +1,22 @@
 import json
-from flask import Flask
+import requests
+
+with open("telegram_code.json", "w") as fp:
+    json.dump({"telegram_code" : "1698758222:AAFbYkMBmDck-bP_wi7vlayqyZ5c0rGn5U0"}, fp)
+
+
+with open("telegram_code.json", "r") as fp:
+    api_key = json.load(fp)["telegram_code"]
+    url = "https://api.telegram.org/bot" + api_key + "/getUpdates"
 
 
 
-app = Flask(__name__)
 
-if __name__ == "__main__":
-    app.run(host="0,0,0,0", port="8080")
+response = requests.get(url)
+response.json()
+result_list = response.json().get("result")
 
-
-@app.route('/')
-def get_bot_info():
-
-    with open("telegram_code.json", "r") as fp:
-        api_key = json.load(fp)["telegram_code"]
-        print(api_key)
+print(result_list)
+for result in result_list:
+    if result.get("chat") == "chat":
+        print("3")
