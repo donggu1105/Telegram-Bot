3f04d6877ceafdd4c34d280331be806c08504920
donggu1105 <kbh11051@gmail.com>
2021-03-09 09:49:58 +0900

    텔레그램 봇 메시지 보내기 완료

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import requests
 token = "1698758222:AAFbYkMBmDck-bP_wi7vlayqyZ5c0rGn5U0"
 mc = "1611694077"
 bot = telepot.Bot(token)
-bot.sendMessage(mc, "wassup")
+bot.sendMessage(mc, "wassup") #
 
 
 
