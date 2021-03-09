79460977ae1cf5af017afb00d180df65951b198d
donggu1105 <kbh11051@gmail.com>
2021-03-09 09:49:14 +0900

    텔레그램 봇 메시지 보내기 완료

--- main.py
+++ main.py
@@ -1,23 +1,11 @@
 import json
+import telepot
 import requests
 
-with open("telegram_code.json", "w") as fp:
-    json.dump({"telegram_code" : "1698758222:AAFbYkMBmDck-bP_wi7vlayqyZ5c0rGn5U0"}, fp)
-
-
-with open("telegram_code.json", "r") as fp:
-    api_key = json.load(fp)["telegram_code"]
-    url = "https://api.telegram.org/bot" + api_key + "/getUpdates"
+token = "1698758222:AAFbYkMBmDck-bP_wi7vlayqyZ5c0rGn5U0"
+mc = "1611694077"
+bot = telepot.Bot(token)
+bot.sendMessage(mc, "wassup")
 
 
 
-
-response = requests.get(url)
-response.json()
-result_list = response.json().get("result")
-
-print(result_list)
-for index, result in enumerate(result_list):
-    print(result.keys())
-    if result.keys() == "message":
-        print(1)
