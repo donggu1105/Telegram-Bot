33875bb707b86bd7159faabae2a98606cdce440c
donggu1105 <kbh11051@gmail.com>
2021-03-09 11:28:45 +0900

    텔레그램 봇 메시지 보내기 완료

--- json_practice.py
+++ json_practice.py
@@ -1,38 +1,13 @@
-import json
+import re
 
-dict1 = {"name" : "song", "age" : 10}
+text = "문의사항이 있으면 032-232-3245 으로 연락주시기 바랍니다."
 
-print("dict1 : %s" % dict1)
-print("dict1 type : %s" % type(dict1))
-
-print("name : %s" %dict1.get("name"))
-print("============")
-
-#convert dictionary to json using json.dump
-json_val = json.dumps(dict1, indent=2)
-
-print("json_val : %s" %json_val)
-print("json_val type : %s" % type(json_val))
-
-print("==============")
-
-dict2 = json.loads(json_val)
-print("dict2 : %s" % dict2)
-print("dict2 type : %s" % type(dict2))
+regex = re.compile(r"")
 
 
-
-######### list
-
-temp = list([2,3,4,5])
-temp_new = list()
-
-for i in temp:
-    temp_new.append(i*2)
-
-print(temp_new)
-
-temp_new2 = list([i*2 for i in temp_new])
-print(temp_new2)
-temp_new3 = list([i**2 for i in temp_new2])
-print(temp_new3)
+# regex = re.compile(r'(\d{3})-(\d{3}-\d{4})')
+# matchobj = regex.search(text)
+# areaCode = matchobj.group(1)
+# num = matchobj.group(2)
+# fullNum = matchobj.group()
+# print(areaCode, num)  # 032 232-3245
--- main.py
+++ main.py
@@ -1,11 +1,21 @@
-import json
 import telepot
-import requests
 
-token = "1698758222:AAFbYkMBmDck-bP_wi7vlayqyZ5c0rGn5U0"
-mc = "1611694077"
-bot = telepot.Bot(token)
-bot.sendMessage(mc, "wassup") #
+token = "1698758222:AAFbYkMBmDck-bP_wi7vlayqyZ5c0rGn5U0" # 봇의 주민등록번호
+mc = "1611694077" # 나의 이름
+bot = telepot.Bot(token) # 봇을 구동하기위해 껍데기 + token = > 진정한 봇
+
+
+def handle(msg):
+
+    print(msg)
+    bot.sendMessage(msg["from"]["id"], msg["text"])
+
+bot.message_loop(handle)
+
+
+
+while True:
+    pass
 
 
 
